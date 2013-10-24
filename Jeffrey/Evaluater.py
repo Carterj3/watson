c51c668cbedde0106a076f8fd74da24cf14d6adb
Jeffrey Carter <carterj3@rose-hulman.edu>
2013-10-23 20:54:09 -0400

    lowered dataset size to 200 instead of 483. runs significantly faster.

--- Jeffrey/Evaluater.py
+++ Jeffrey/Evaluater.py
@@ -3,20 +3,30 @@ Created on Oct 20, 2013
 
 @author: Carterj3
 '''
+from time import clock
 
 if __name__ == '__main__':
     pass
-  
+
+def jeff_cube(x):
+  return (x**3)
   
 def jeff_square(x):
   return (x**2)
 
 # takes a row, weights, and func. Evaluates every row and returns their weighted sum
-def jeff_eval_row(row,weights,func=jeff_square):
+def jeff_eval_row(row,weights,func=jeff_cube):
   s = 0
   for i in range(0,len(row)):
     s += weights[i] * func(row[i])
   return s
+
+def jeff_eval_qid(qid,weights,func=jeff_square):
+  temp = []
+  for i in range(0,len(qid)):
+    row = qid[i]
+    temp.append((row[0],jeff_eval_row(row[2:],weights,func)))
+  return temp
 
 def jeff_is_answer_row(row):
   return (row[len(row)-1])
@@ -37,8 +47,6 @@ def jeff_eval_weight(dataset,weights_score):
     question_size = 0
     while j < len(dataset) and dataset[j][1] == dataset[start][1]:
       j = j + 1
-      if j == 85:
-        pass
     answers = []
     while i < j:
       question_size = question_size + 1
@@ -54,4 +62,47 @@ def jeff_eval_weight(dataset,weights_score):
         weight_score += k - start
         
   return weight_score
+
+def jeff_create_question_array(dataset):
+  i = 0
+  j = 0
+  master_qids = []
+  while i < len(dataset):
+    t1 = clock()
+    start = i
+    qids = []
+    while i < len(dataset) and dataset[i][1]== dataset[start][1]:
+      qids.append(dataset[i])
+      i = i +1
+    master_qids.append(qids)
+    print "Create_QIDs",i,"/",len(dataset),"|",clock()-t1
+    t1 = clock()
+    
+  return master_qids
+
+
+def jeff_save_n_qid(dataset,n,m=0):
+  qids = jeff_create_question_array(dataset)
+  
+  from cStringIO import StringIO
+  tempstring = StringIO()
+  
+  i = m
+  while i < n and i <len(qids):
+    t1 = clock()
+    q = qids[i]
+    for row in q:
+      for l in range(0,len(row)-1):
+        value = str(row[l])
+        tempstring.write(value+",")
+      tempstring.write(str(row[len(row)-1])+"\n")
+    #qids[i] = []
+    print "Save-QIDS",i,"|",clock()-t1
+    t1 = clock()
+    i = i +1
+    
+  with open("tgmc_"+str(n)+".csv",'w') as f:
+    f.write(tempstring.getvalue())
+    
+    
       
--- Jeffrey/GA.py
+++ Jeffrey/GA.py
@@ -3,33 +3,41 @@ Created on Oct 20, 2013
 
 @author: Carterj3
 '''
+
+train = True
+
 weight_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\jeff_weights'
 
-dataset_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmctrain.csv'
+eval_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmcevaluation.csv'
+
+#dataset_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmctrain.csv'
+dataset_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmc_200.csv'
 #dataset_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmctrain-1q.csv'
 #dataset_file = 'C:\\Users\\Carterj3\\workspace\\ai\\AI-Watson\\tgmctrain-2q.csv'
 
 from multiprocessing import Queue
 from threading import Thread
 from random import random,uniform
-from Evaluater import jeff_eval_row,jeff_eval_weight
+from Evaluater import jeff_eval_row,jeff_eval_weight,jeff_create_question_array,jeff_eval_qid
 from types import BooleanType
 from time import clock
 
-def get_dataset():
+def get_dataset(d_file=dataset_file):
   # Store weights in the same file
   dataset =  [];
   i = 0
   from time import clock
   t1 = clock()
-  with open(dataset_file,'r') as f:
+  with open(d_file,'r') as f:
     for line in f:
       temp = []
-      for value in line.strip('\n').split(','):
-        if not (value in ('true','false')):
+      for tValue in line.strip('\n').split(','):
+        value = tValue.strip()
+        if not (value in ('true','false','True','False')):
           temp.append(float(value))
         else:
-          temp.append(value == 'true')
+          temp.append((value == 'true' or value == 'True'))
+
       dataset.append(temp)
       i = i+1
       if (i %1000) == 0:
@@ -40,9 +48,12 @@ def get_dataset():
 def get_weights():
   # Store weights in the same file
   weights =  None;
+  score = 0
   with open(weight_file,'r') as f:
     for line in f:
-      temp_line = line.split(":")[1]
+      temp = line.split(":")
+      score = float(temp[0])
+      temp_line = temp[1]
       weights = temp_line.strip('\n').split(',')
   temp = []
   for each in weights:
@@ -52,7 +63,7 @@ def get_weights():
       # I don't store the weights well so it goes like 1,1,1,
       # and that last , screws everybody up
       pass
-  return temp
+  return score,temp
   
 def store_weights(weights,rating):
   from cStringIO import StringIO
@@ -66,6 +77,22 @@ def store_weights(weights,rating):
   
   with open(weight_file,'a') as f:
     f.write(tempstring.getvalue())
+
+
+def store_answers(answers):
+  from cStringIO import StringIO
+  tempstring = StringIO()
+  
+  for l in range(0,len(answers)-1):
+    value = str(answers[l])
+    tempstring.write(value+"\n")
+  tempstring.write(str(answers[len(answers)-1]))
+  
+  with open("answers.txt",'w') as f:
+    temp = tempstring.getvalue()
+    f.write(temp)
+
+
 
 def runHelper_jeff_eval_row(dataset,rowIndex,weights,queue,t1):
   if type(dataset[rowIndex][len(dataset[rowIndex])-1]) is BooleanType:
@@ -87,10 +114,10 @@ def runHelper_jeff_eval_weight(each_weight,each_score,dataset,weight_queue):
     ls.append(temp_weight)
     weight_queue.put(ls)
 
-def run(dataset,count=10,mutate=.01,diviance=.30,):
-  
+def Train_run(dataset,count=100,mutate=.10,diviance=.30):
+  t1 = clock()
   # Load the weights
-  w = get_weights()
+  score,w = get_weights()
   
   # (weights, weights_score)
   temp_all_weights = Queue()
@@ -126,8 +153,8 @@ def run(dataset,count=10,mutate=.01,diviance=.30,):
       end_times.append(clock())
       total_time = total_time + end_times[j] - start_times[j]
       
-    print "S_Threads took ", total_time / len(dataset)," seconds (AVG)"
-    print "S_Threads took ", total_time, " seconds (Total)"
+    print "S_Threads took ", total_time / len(dataset)," seconds (AVG)",i
+    print "S_Threads took ", total_time, " seconds (Total)",i
     
     weights_score = []
     while not temp_weights_score.empty():
@@ -162,7 +189,7 @@ def run(dataset,count=10,mutate=.01,diviance=.30,):
   print "W_Threads took ", total_time / len(dataset)," seconds (AVG)"
   print "W_Threads took ", total_time, " seconds (Total)" 
   #
-  max_rating, max_weight = 0,[]
+  max_rating, max_weight = -1,[]
   while not weight_queue.empty():
     temp_rating,temp_weight = weight_queue.get()
     if temp_rating > max_rating:
@@ -171,14 +198,41 @@ def run(dataset,count=10,mutate=.01,diviance=.30,):
     
       
   
-  # Store best weight
+  # Store best weight fi it was better than the old one
+  if score < max_rating:
+    store_weights(max_weight,max_rating)
+  print "Storing weight with a value of",max_rating,"took",clock()-t1,"seconds"
   
-  store_weights(max_weight,max_rating)
-  print "Storing weight with a value of",max_rating
+def Eval_run():
+  t1 = clock()
+  # Load the weights
+  score,w = get_weights()
   
+  eval = get_dataset(eval_file)
   
+  eval_qids = jeff_create_question_array(eval)
+  
+  answers = []
+  t1 = clock()
+  for i in range(0,len(eval_qids)):
+    qid = eval_qids[i]
+    temp = jeff_eval_qid(qid,w)
+    sorted_temp = sorted(temp,key=lambda x: x[1])
+    answers.append(sorted_temp[len(sorted_temp)-1][0])
+    if (i % 1) == 0:
+      print "qids",i+1,"/",len(eval_qids),"|",clock()-t1,"seconds"
+      t1 = clock()
+    
+  store_answers(answers)
+  
+    
 if __name__ == '__main__':
-  data = get_dataset()
-  while True:
-    run(data)
-    break
+  if train:
+    data = get_dataset()
+    while True:
+      Train_run(data)
+  else:
+    #Eval_run()
+    from Evaluater import jeff_save_n_qid
+    data = get_dataset()
+    jeff_save_n_qid(data,200)
